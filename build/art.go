464f17a661cf088a5d2f2de0994b71d51ae3ba68
Jing Ji <a5705c@motorola.com>
2015-03-12 10:52:14 -0500

    ART Compilation RAM Usage Optimization
    This patch include two optimizations
    * In the compilation phase, flush the swap-backed compilation
      data into disk. The rational of this change is that, the arena
      allocator creates a 16MB new buffer everytime if there is no
      enough buffer; the old buffer is not needed until the oat
      write phase. So flushing the data into disk would keep the PSS
      in compilation phase even.
    * Right before writing the oat file, shutdown the VM heap. The
      VM heap is used to load classes during compilation, it's not
      used after the compilation is done. Shutting it down will save
      RAM in oat write phase apparently. Meanwhile the aforementioned
      swap-backed data will be reloaded into RAM in this phase, so
      the total RAM usage might be around the same level as in the
      compilation phase.
    
    Taking Facebook app as an example, tested in Clark:
    * The peak PSS of dex2oat
      w/o this patch: 287MB
      w/ this patch:  223MB
      - this patch saves 64MB
    * The time used in compilation
      w/o this patch: 39s
      w/ this patch:  47s
      - this patch costs 8 more seconds
    
    Change-Id: I37799598e239ec0dc6402731ee0d19ae2cc0ed61
    Reviewed-on: http://gerrit.mot.com/724301
    Tested-by: Jira Key <jirakey@motorola.com>
    SME-Granted: Gerrit Application <gerritmailarchive@motorola.com>
    Reviewed-by: Udayan Banerji <ubanerji@motorola.com>
    Reviewed-by: Ye Ouyang <yeouyang@motorola.com>
    Reviewed-by: Xin Guan <a18772@motorola.com>
    SLTApproved: Xin Guan <a18772@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/763695
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jing Ji <a5705c@motorola.com>
    Submit-Approved: Jing Ji <a5705c@motorola.com>
    Tested-by: Jing Ji <a5705c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/855885
    Reviewed-on: https://gerrit.mot.com/1018838
    SLTApproved: Slta Waiver
    Reviewed-by: Xiu Jin Wang <androider@motorola.com>
    Submit-Approved: Jira Key
    SLTApproved: Ye Ouyang <yeouyang@motorola.com>
    SME-Granted: Ye Ouyang <yeouyang@motorola.com>
    Submit-Approved: Ye Ouyang <yeouyang@motorola.com>
    Tested-by: Ye Ouyang <yeouyang@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1356302
    Reviewed-by: Maoyuan Wu <wumy@motorola.com>
    Submit-Approved: Maoyuan Wu <wumy@motorola.com>
    Tested-by: Maoyuan Wu <wumy@motorola.com>

--- build/art.go
+++ build/art.go
@@ -144,6 +144,11 @@ func deviceFlags(ctx android.BaseContext) []string {
 		cflags = append(cflags, "-DART_TARGET_LINUX")
 	} else {
 		cflags = append(cflags, "-DART_TARGET_ANDROID")
+		// BEGIN Motorola, a5705c, 03/12/2015, IKVPREL1L-8365
+		if envTrue(ctx, "WITH_MOTO_ART_COMPILER_MEM_OPT") {
+			cflags = append(cflags, "-DMOTO_ART_COMPILER_MEM_OPT=1")
+		}
+		// END IKVPREL1L-8365
 	}
 	minDelta := envDefault(ctx, "LIBART_IMG_TARGET_MIN_BASE_ADDRESS_DELTA", "-0x1000000")
 	maxDelta := envDefault(ctx, "LIBART_IMG_TARGET_MAX_BASE_ADDRESS_DELTA", "0x1000000")

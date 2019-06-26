bionic-linux/android_art

Make the JIT zygote memory shared.
Test: boots
Bug: 119800099
Change-Id: I75ff8a58eea4de5cb833139641b4e15b8394d9b1

runtime/jit/jit_memory_region.h
@@ -67,6 +67,10 @@ class JitMemoryRegion {
   uint8_t* AllocateData(size_t data_size) REQUIRES(Locks::jit_lock_);
   void FreeData(uint8_t* data) REQUIRES(Locks::jit_lock_);
 
+  bool IsInvalid() const NO_THREAD_SAFETY_ANALYSIS {
+    return exec_mspace_ == nullptr && data_mspace_ == nullptr;
+  }
+
   bool HasDualCodeMapping() const {
     return non_exec_pages_.IsValid();
   }

MotorolaMobilityLLC/art

runtime/jit/jit_memory_region.h
@@ -107,6 +107,10 @@ class JitMemoryRegion {
       REQUIRES(Locks::jit_lock_)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
+  bool IsValid() const NO_THREAD_SAFETY_ANALYSIS {
+    return exec_mspace_ != nullptr || data_mspace_ != nullptr;
+  }
+
   bool HasDualCodeMapping() const {
     return non_exec_pages_.IsValid();
   }
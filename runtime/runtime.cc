edb1a63633ecb045b57cd7e6fc3dd1d272468dda
Orion Hodson <oth@google.com>
2019-07-18 12:57:47 +0100

    [ALPS04821230] Refactor Core Plaform API checking
    The change modified from master@1062191 is only available for Q.
    __builtin_return_address may return an wrong value that will cause
    some CTS cases failing, e.g.,:
      - CtsHiddenApiBlacklistApi27TestCases
      - CtsHiddenApiBlacklistApi28TestCases
    
    Move Core Platform API check logic to hidden_api_jni.{h,cc}.
    
    Use libunwindstack for walking stack as builtin isn't robust without
    frame pointers. This is moderately expensive so we currently walk a
    maximum of 3 frames.
    
    Checks are now performed by placing ScopedCorePlatformApiCheck
    instances at potential points of entry via JNI. Specifically, where
    JNI interfaces implement methods for getting field and method ids. The
    check is only performed on the outermost instance of the
    ScopedCorePlatformApiCheck since only the outer caller is of interest.
    
    State for the checks is stored in the current Thread instance since
    the JNI interface signatures are fixed and we need a way to be able to
    determine whether a ScopedCorePlatformApiCheck is the outermost
    instance and whether the outmost instance was approved.
    
    Test: build eng build (has checkjni), boot, grep logcat nio/Buffer
    Test: build user build (regular jni), boot, grep logcat nio/Buffer
    Test: CtsHiddenApiBlacklistApi27TestCases 
    Test: CtsHiddenApiBlacklistApi28TestCases
    Bug: 130336799
    Bug: 136276414
    Change-Id: If1261046dd57c007bc77b213daaa5f733151123e
    Feature: Google Patch
    CR-Id: ALPS04821230

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -86,6 +86,7 @@
 #include "gc/system_weak.h"
 #include "handle_scope-inl.h"
 #include "hidden_api.h"
+#include "hidden_api_jni.h"
 #include "image-inl.h"
 #include "instrumentation.h"
 #include "intern_table-inl.h"
@@ -493,7 +494,7 @@ Runtime::~Runtime() {
   // elements of WellKnownClasses to be null, see b/65500943.
   WellKnownClasses::Clear();
 
-  JniShutdownNativeCallerCheck();
+  hiddenapi::JniShutdownNativeCallerCheck();
 }
 
 struct AbortState {
@@ -1841,7 +1842,7 @@ void Runtime::InitNativeMethods() {
 
   // Having loaded native libraries for Managed Core library, enable field and
   // method resolution checks via JNI from native code.
-  JniInitializeNativeCallerCheck();
+  hiddenapi::JniInitializeNativeCallerCheck();
 
   VLOG(startup) << "Runtime::InitNativeMethods exiting";
 }

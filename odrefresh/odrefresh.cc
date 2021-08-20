bionic-linux/android_art

Add unit tests for odrefresh - step 2
- Define MockableExecUtils, which is a wrapper class to make exec_utils
mockable.
- Add another constructor to the OnDeviceRefresh class to support
injections.
- Update OdrConfig to support overriding the staging directory.
- Add a unit test to verify that odrefresh sets compiler filter based on
"dalvik.vm.systemservercompilerfilter".

Bug: 196188549
Test: atest art_standalone_odrefresh_tests
Test: atest odsign_e2e_tests
Change-Id: Ia7b4347bba6873878490a513b8a5ff6642122d71

odrefresh/odrefresh.cc
@@ -65,8 +65,8 @@
 #include "com_android_art.h"
 #include "dex/art_dex_file_loader.h"
 #include "dexoptanalyzer.h"
-#include "exec_utils.h"
 #include "log/log.h"
+#include "mockable_exec_utils.h"
 #include "odr_artifacts.h"
 #include "odr_common.h"
 #include "odr_compilation_log.h"
@@ -456,9 +456,17 @@ std::string GetBootImage() {
 }  // namespace
 
 OnDeviceRefresh::OnDeviceRefresh(const OdrConfig& config)
+    : OnDeviceRefresh(config,
+                      Concatenate({kOdrefreshArtifactDirectory, "/", kCacheInfoFile}),
+                      std::make_unique<MockableExecUtils>()) {}
+
+OnDeviceRefresh::OnDeviceRefresh(const OdrConfig& config,
+                                 const std::string& cache_info_filename,
+                                 std::unique_ptr<MockableExecUtils> exec_utils)
     : config_{config},
-      cache_info_filename_{Concatenate({kOdrefreshArtifactDirectory, "/", kCacheInfoFile})},
-      start_time_{time(nullptr)} {
+      cache_info_filename_{cache_info_filename},
+      start_time_{time(nullptr)},
+      exec_utils_{std::move(exec_utils)} {
   for (const std::string& jar : android::base::Split(config_.GetDex2oatBootClasspath(), ":")) {
     // Boot class path extensions are those not in the ART APEX. Updatable APEXes should not
     // have DEX files in the DEX2OATBOOTCLASSPATH. At the time of writing i18n is a non-updatable
@@ -1077,7 +1085,8 @@ WARN_UNUSED bool OnDeviceRefresh::VerifyBootExtensionArtifactsAreUpToDate(const 
   std::string error_msg;
   bool timed_out = false;
   const time_t timeout = GetSubprocessTimeout();
-  const int dexoptanalyzer_result = ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
+  const int dexoptanalyzer_result =
+      exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
   if (dexoptanalyzer_result == -1) {
     LOG(ERROR) << "Unexpected exit from dexoptanalyzer: " << error_msg;
     if (timed_out) {
@@ -1173,7 +1182,8 @@ WARN_UNUSED bool OnDeviceRefresh::VerifySystemServerArtifactsAreUpToDate(bool on
     std::string error_msg;
     bool timed_out = false;
     const time_t timeout = GetSubprocessTimeout();
-    const int dexoptanalyzer_result = ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
+    const int dexoptanalyzer_result =
+        exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
     if (dexoptanalyzer_result == -1) {
       LOG(ERROR) << "Unexpected exit from dexoptanalyzer: " << error_msg;
       if (timed_out) {
@@ -1352,7 +1362,7 @@ WARN_UNUSED bool OnDeviceRefresh::CompileBootExtensionArtifacts(const Instructio
   }
 
   bool timed_out = false;
-  int dex2oat_exit_code = ExecAndReturnCode(args, timeout, &timed_out, error_msg);
+  int dex2oat_exit_code = exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, error_msg);
   if (dex2oat_exit_code != 0) {
     if (timed_out) {
       metrics.SetStatus(OdrMetrics::Status::kTimeLimitExceeded);
@@ -1502,7 +1512,7 @@ WARN_UNUSED bool OnDeviceRefresh::CompileSystemServerArtifacts(const std::string
     }
 
     bool timed_out = false;
-    int dex2oat_exit_code = ExecAndReturnCode(args, timeout, &timed_out, error_msg);
+    int dex2oat_exit_code = exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, error_msg);
     if (dex2oat_exit_code != 0) {
       if (timed_out) {
         metrics.SetStatus(OdrMetrics::Status::kTimeLimitExceeded);
@@ -1533,10 +1543,14 @@ OnDeviceRefresh::Compile(OdrMetrics& metrics,
   const char* staging_dir = nullptr;
   metrics.SetStage(OdrMetrics::Stage::kPreparation);
 
-  // Create staging area and assign label for generating compilation artifacts.
-  if (PaletteCreateOdrefreshStagingDirectory(&staging_dir) != PALETTE_STATUS_OK) {
-    metrics.SetStatus(OdrMetrics::Status::kStagingFailed);
-    return ExitCode::kCleanupFailed;
+  if (!config_.GetStagingDir().empty()) {
+    staging_dir = config_.GetStagingDir().c_str();
+  } else {
+    // Create staging area and assign label for generating compilation artifacts.
+    if (PaletteCreateOdrefreshStagingDirectory(&staging_dir) != PALETTE_STATUS_OK) {
+      metrics.SetStatus(OdrMetrics::Status::kStagingFailed);
+      return ExitCode::kCleanupFailed;
+    }
   }
 
   // Emit cache info before compiling. This can be used to throttle compilation attempts later.

odrefresh/odrefresh.h
@@ -18,12 +18,14 @@
 #define ART_ODREFRESH_ODREFRESH_H_
 
 #include <ctime>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
 
 #include "com_android_apex.h"
 #include "com_android_art.h"
+#include "mockable_exec_utils.h"
 #include "odr_artifacts.h"
 #include "odr_config.h"
 #include "odr_metrics.h"
@@ -35,6 +37,11 @@ namespace odrefresh {
 class OnDeviceRefresh final {
  public:
   explicit OnDeviceRefresh(const OdrConfig& config);
+
+  // Constructor with injections. For testing and internal use only.
+  OnDeviceRefresh(const OdrConfig& config,
+                  const std::string& cache_info_filename,
+                  std::unique_ptr<MockableExecUtils> exec_utils);
 
   // Returns the exit code, a list of ISAs that boot extensions should be compiled for, and a
   // boolean indicating whether the system server should be compiled.
@@ -168,6 +175,8 @@ class OnDeviceRefresh final {
   std::vector<std::string> boot_classpath_jars_;
 
   const time_t start_time_;
+
+  std::unique_ptr<MockableExecUtils> exec_utils_;
 
   DISALLOW_COPY_AND_ASSIGN(OnDeviceRefresh);
 };

odrefresh/odrefresh_test.cc
@@ -14,19 +14,200 @@
  * limitations under the License.
  */
 
-#include "odrefresh/odrefresh.h"
+#include "odrefresh.h"
 
+#include <functional>
+#include <memory>
+#include <string_view>
+
+#include "android-base/properties.h"
+#include "android-base/scopeguard.h"
+#include "android-base/strings.h"
+#include "arch/instruction_set.h"
 #include "base/common_art_test.h"
 #include "base/file_utils.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "mockable_exec_utils.h"
+#include "odr_common.h"
+#include "odr_config.h"
+#include "odr_fs_utils.h"
+#include "odr_metrics.h"
+#include "odrefresh/odrefresh.h"
 
 namespace art {
 namespace odrefresh {
 
-TEST(OdRefreshTest, OdrefreshArtifactDirectory) {
-    // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
-    // where compilation artifacts end up.
-    ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
-    EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
+using ::testing::AllOf;
+using ::testing::Contains;
+using ::testing::HasSubstr;
+using ::testing::Return;
+
+constexpr int kReplace = 1;
+
+void CreateEmptyFile(const std::string& name) {
+  File* file = OS::CreateEmptyFile(name.c_str());
+  file->Release();
+  delete file;
+}
+
+android::base::ScopeGuard<std::function<void()>> ScopedSetProperty(const std::string& key,
+                                                                   const std::string& value) {
+  std::string old_value = android::base::GetProperty(key, /*default_value=*/{});
+  android::base::SetProperty(key, value);
+  return android::base::ScopeGuard([=]() { android::base::SetProperty(key, old_value); });
+}
+
+class MockExecUtils : public MockableExecUtils {
+ public:
+  // A workaround to avoid MOCK_METHOD on a method with an `std::string*` parameter, which will lead
+  // to a conflict between gmock and android-base/logging.h (b/132668253).
+  int ExecAndReturnCode(std::vector<std::string>& arg_vector,
+                        time_t,
+                        bool*,
+                        std::string*) const override {
+    return DoExecAndReturnCode(arg_vector);
+  }
+
+  MOCK_METHOD(int, DoExecAndReturnCode, (std::vector<std::string> & arg_vector), (const));
+};
+
+class OdRefreshTest : public CommonArtTest {
+ public:
+  OdRefreshTest() : config_("odrefresh") {}
+
+ protected:
+  void SetUp() override {
+    CommonArtTest::SetUp();
+
+    temp_dir_ = std::make_unique<ScratchDir>();
+    std::string_view temp_dir_path = temp_dir_->GetPath();
+    android::base::ConsumeSuffix(&temp_dir_path, "/");
+
+    std::string android_root_path = Concatenate({temp_dir_path, "/system"});
+    ASSERT_TRUE(EnsureDirectoryExists(android_root_path));
+    android_root_env_ = std::make_unique<ScopedUnsetEnvironmentVariable>("ANDROID_ROOT");
+    setenv("ANDROID_ROOT", android_root_path.c_str(), kReplace);
+
+    std::string art_apex_data_path = Concatenate({temp_dir_path, kArtApexDataDefaultPath});
+    ASSERT_TRUE(EnsureDirectoryExists(art_apex_data_path));
+    art_apex_data_env_ = std::make_unique<ScopedUnsetEnvironmentVariable>("ART_APEX_DATA");
+    setenv("ART_APEX_DATA", art_apex_data_path.c_str(), kReplace);
+
+    std::string dalvik_cache_dir = art_apex_data_path + "/dalvik-cache";
+    ASSERT_TRUE(EnsureDirectoryExists(dalvik_cache_dir));
+
+    std::string framework_dir = android_root_path + "/framework";
+    framework_jar_ = framework_dir + "/framework.jar";
+    location_provider_jar_ = framework_dir + "/com.android.location.provider.jar";
+    services_jar_ = framework_dir + "/services.jar";
+    std::string services_jar_prof = framework_dir + "/services.jar.prof";
+
+    // Create placeholder files.
+    ASSERT_TRUE(EnsureDirectoryExists(framework_dir));
+    CreateEmptyFile(framework_jar_);
+    CreateEmptyFile(location_provider_jar_);
+    CreateEmptyFile(services_jar_);
+    CreateEmptyFile(services_jar_prof);
+
+    config_.SetApexInfoListFile(Concatenate({temp_dir_path, "/apex-info-list.xml"}));
+    config_.SetArtBinDir(Concatenate({temp_dir_path, "/bin"}));
+    config_.SetBootClasspath(framework_jar_);
+    config_.SetDex2oatBootclasspath(framework_jar_);
+    config_.SetSystemServerClasspath(Concatenate({location_provider_jar_, ":", services_jar_}));
+    config_.SetIsa(InstructionSet::kX86_64);
+    config_.SetZygoteKind(ZygoteKind::kZygote64_32);
+
+    std::string staging_dir = dalvik_cache_dir + "/staging";
+    ASSERT_TRUE(EnsureDirectoryExists(staging_dir));
+    config_.SetStagingDir(staging_dir);
+
+    auto mock_exec_utils = std::make_unique<MockExecUtils>();
+    mock_exec_utils_ = mock_exec_utils.get();
+
+    metrics_ = std::make_unique<OdrMetrics>(dalvik_cache_dir);
+    odrefresh_ = std::make_unique<OnDeviceRefresh>(
+        config_, dalvik_cache_dir + "/cache-info.xml", std::move(mock_exec_utils));
+  }
+
+  void TearDown() override {
+    metrics_.reset();
+    temp_dir_.reset();
+    android_root_env_.reset();
+    art_apex_data_env_.reset();
+
+    CommonArtTest::TearDown();
+  }
+
+  std::unique_ptr<ScratchDir> temp_dir_;
+  std::unique_ptr<ScopedUnsetEnvironmentVariable> android_root_env_;
+  std::unique_ptr<ScopedUnsetEnvironmentVariable> art_apex_data_env_;
+  OdrConfig config_;
+  MockExecUtils* mock_exec_utils_;
+  std::unique_ptr<OdrMetrics> metrics_;
+  std::unique_ptr<OnDeviceRefresh> odrefresh_;
+  std::string framework_jar_;
+  std::string location_provider_jar_;
+  std::string services_jar_;
+};
+
+TEST_F(OdRefreshTest, OdrefreshArtifactDirectory) {
+  // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
+  // where compilation artifacts end up.
+  ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
+  EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
+}
+
+TEST_F(OdRefreshTest, CompileSetsCompilerFilter) {
+  {
+    // Defaults to "speed".
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
+
+  {
+    auto guard = ScopedSetProperty("dalvik.vm.systemservercompilerfilter", "speed-profile");
+    // services.jar has a profile, while location.provider.jar does not.
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Contains(HasSubstr("--profile-file-fd=")),
+                                          Contains("--compiler-filter=speed-profile"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
+
+  {
+    auto guard = ScopedSetProperty("dalvik.vm.systemservercompilerfilter", "verify");
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Contains("--compiler-filter=verify"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Contains("--compiler-filter=verify"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
 }
 
 }  // namespace odrefresh

MotorolaMobilityLLC/art

odrefresh/odrefresh.cc
@@ -456,9 +456,17 @@ std::string GetBootImage() {
 }  // namespace
 
 OnDeviceRefresh::OnDeviceRefresh(const OdrConfig& config)
+    : OnDeviceRefresh(config,
+                      Concatenate({kOdrefreshArtifactDirectory, "/", kCacheInfoFile}),
+                      std::make_unique<ExecUtils>()) {}
+
+OnDeviceRefresh::OnDeviceRefresh(const OdrConfig& config,
+                                 const std::string& cache_info_filename,
+                                 std::unique_ptr<ExecUtils> exec_utils)
     : config_{config},
-      cache_info_filename_{Concatenate({kOdrefreshArtifactDirectory, "/", kCacheInfoFile})},
-      start_time_{time(nullptr)} {
+      cache_info_filename_{cache_info_filename},
+      start_time_{time(nullptr)},
+      exec_utils_{std::move(exec_utils)} {
   for (const std::string& jar : android::base::Split(config_.GetDex2oatBootClasspath(), ":")) {
     // Boot class path extensions are those not in the ART APEX. Updatable APEXes should not
     // have DEX files in the DEX2OATBOOTCLASSPATH. At the time of writing i18n is a non-updatable
@@ -1077,7 +1085,8 @@ WARN_UNUSED bool OnDeviceRefresh::VerifyBootExtensionArtifactsAreUpToDate(const 
   std::string error_msg;
   bool timed_out = false;
   const time_t timeout = GetSubprocessTimeout();
-  const int dexoptanalyzer_result = ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
+  const int dexoptanalyzer_result =
+      exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
   if (dexoptanalyzer_result == -1) {
     LOG(ERROR) << "Unexpected exit from dexoptanalyzer: " << error_msg;
     if (timed_out) {
@@ -1173,7 +1182,8 @@ WARN_UNUSED bool OnDeviceRefresh::VerifySystemServerArtifactsAreUpToDate(bool on
     std::string error_msg;
     bool timed_out = false;
     const time_t timeout = GetSubprocessTimeout();
-    const int dexoptanalyzer_result = ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
+    const int dexoptanalyzer_result =
+        exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, &error_msg);
     if (dexoptanalyzer_result == -1) {
       LOG(ERROR) << "Unexpected exit from dexoptanalyzer: " << error_msg;
       if (timed_out) {
@@ -1352,7 +1362,7 @@ WARN_UNUSED bool OnDeviceRefresh::CompileBootExtensionArtifacts(const Instructio
   }
 
   bool timed_out = false;
-  int dex2oat_exit_code = ExecAndReturnCode(args, timeout, &timed_out, error_msg);
+  int dex2oat_exit_code = exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, error_msg);
   if (dex2oat_exit_code != 0) {
     if (timed_out) {
       metrics.SetStatus(OdrMetrics::Status::kTimeLimitExceeded);
@@ -1502,7 +1512,7 @@ WARN_UNUSED bool OnDeviceRefresh::CompileSystemServerArtifacts(const std::string
     }
 
     bool timed_out = false;
-    int dex2oat_exit_code = ExecAndReturnCode(args, timeout, &timed_out, error_msg);
+    int dex2oat_exit_code = exec_utils_->ExecAndReturnCode(args, timeout, &timed_out, error_msg);
     if (dex2oat_exit_code != 0) {
       if (timed_out) {
         metrics.SetStatus(OdrMetrics::Status::kTimeLimitExceeded);
@@ -1533,10 +1543,14 @@ OnDeviceRefresh::Compile(OdrMetrics& metrics,
   const char* staging_dir = nullptr;
   metrics.SetStage(OdrMetrics::Stage::kPreparation);
 
-  // Create staging area and assign label for generating compilation artifacts.
-  if (PaletteCreateOdrefreshStagingDirectory(&staging_dir) != PALETTE_STATUS_OK) {
-    metrics.SetStatus(OdrMetrics::Status::kStagingFailed);
-    return ExitCode::kCleanupFailed;
+  if (!config_.GetStagingDir().empty()) {
+    staging_dir = config_.GetStagingDir().c_str();
+  } else {
+    // Create staging area and assign label for generating compilation artifacts.
+    if (PaletteCreateOdrefreshStagingDirectory(&staging_dir) != PALETTE_STATUS_OK) {
+      metrics.SetStatus(OdrMetrics::Status::kStagingFailed);
+      return ExitCode::kCleanupFailed;
+    }
   }
 
   // Emit cache info before compiling. This can be used to throttle compilation attempts later.

odrefresh/odrefresh.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,12 +18,14 @@
 #define ART_ODREFRESH_ODREFRESH_H_
 
 #include <ctime>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
 
 #include "com_android_apex.h"
 #include "com_android_art.h"
+#include "exec_utils.h"
 #include "odr_artifacts.h"
 #include "odr_config.h"
 #include "odr_metrics.h"
@@ -35,6 +37,11 @@ namespace odrefresh {
 class OnDeviceRefresh final {
  public:
   explicit OnDeviceRefresh(const OdrConfig& config);
+
+  // Constructor with injections. For testing and internal use only.
+  OnDeviceRefresh(const OdrConfig& config,
+                  const std::string& cache_info_filename,
+                  std::unique_ptr<ExecUtils> exec_utils);
 
   // Returns the exit code, a list of ISAs that boot extensions should be compiled for, and a
   // boolean indicating whether the system server should be compiled.
@@ -168,6 +175,8 @@ class OnDeviceRefresh final {
   std::vector<std::string> boot_classpath_jars_;
 
   const time_t start_time_;
+
+  std::unique_ptr<ExecUtils> exec_utils_;
 
   DISALLOW_COPY_AND_ASSIGN(OnDeviceRefresh);
 };

odrefresh/odrefresh_test.cc
@@ -14,19 +14,218 @@
  * limitations under the License.
  */
 
-#include "odrefresh/odrefresh.h"
+#include "odrefresh.h"
 
+#include <functional>
+#include <memory>
+#include <string_view>
+
+#include "android-base/properties.h"
+#include "android-base/scopeguard.h"
+#include "android-base/strings.h"
+#include "arch/instruction_set.h"
 #include "base/common_art_test.h"
 #include "base/file_utils.h"
+#include "exec_utils.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "odr_common.h"
+#include "odr_config.h"
+#include "odr_fs_utils.h"
+#include "odr_metrics.h"
+#include "odrefresh/odrefresh.h"
 
 namespace art {
 namespace odrefresh {
 
-TEST(OdRefreshTest, OdrefreshArtifactDirectory) {
-    // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
-    // where compilation artifacts end up.
-    ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
-    EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
+using ::testing::AllOf;
+using ::testing::Contains;
+using ::testing::HasSubstr;
+using ::testing::Not;
+using ::testing::Return;
+
+constexpr int kReplace = 1;
+
+void CreateEmptyFile(const std::string& name) {
+  File* file = OS::CreateEmptyFile(name.c_str());
+  ASSERT_TRUE(file != nullptr);
+  file->Release();
+  delete file;
+}
+
+android::base::ScopeGuard<std::function<void()>> ScopedSetProperty(const std::string& key,
+                                                                   const std::string& value) {
+  std::string old_value = android::base::GetProperty(key, /*default_value=*/{});
+  android::base::SetProperty(key, value);
+  return android::base::ScopeGuard([=]() { android::base::SetProperty(key, old_value); });
+}
+
+class MockExecUtils : public ExecUtils {
+ public:
+  // A workaround to avoid MOCK_METHOD on a method with an `std::string*` parameter, which will lead
+  // to a conflict between gmock and android-base/logging.h (b/132668253).
+  int ExecAndReturnCode(std::vector<std::string>& arg_vector,
+                        time_t,
+                        bool*,
+                        std::string*) const override {
+    return DoExecAndReturnCode(arg_vector);
+  }
+
+  MOCK_METHOD(int, DoExecAndReturnCode, (std::vector<std::string> & arg_vector), (const));
+};
+
+class OdRefreshTest : public CommonArtTest {
+ public:
+  OdRefreshTest() : config_("odrefresh") {}
+
+ protected:
+  void SetUp() override {
+    CommonArtTest::SetUp();
+
+    temp_dir_ = std::make_unique<ScratchDir>();
+    std::string_view temp_dir_path = temp_dir_->GetPath();
+    android::base::ConsumeSuffix(&temp_dir_path, "/");
+
+    std::string android_root_path = Concatenate({temp_dir_path, "/system"});
+    ASSERT_TRUE(EnsureDirectoryExists(android_root_path));
+    android_root_env_ = std::make_unique<ScopedUnsetEnvironmentVariable>("ANDROID_ROOT");
+    setenv("ANDROID_ROOT", android_root_path.c_str(), kReplace);
+
+    std::string android_art_root_path = Concatenate({temp_dir_path, "/apex/com.android.art"});
+    ASSERT_TRUE(EnsureDirectoryExists(android_art_root_path));
+    android_art_root_env_ = std::make_unique<ScopedUnsetEnvironmentVariable>("ANDROID_ART_ROOT");
+    setenv("ANDROID_ART_ROOT", android_art_root_path.c_str(), kReplace);
+
+    std::string art_apex_data_path = Concatenate({temp_dir_path, kArtApexDataDefaultPath});
+    ASSERT_TRUE(EnsureDirectoryExists(art_apex_data_path));
+    art_apex_data_env_ = std::make_unique<ScopedUnsetEnvironmentVariable>("ART_APEX_DATA");
+    setenv("ART_APEX_DATA", art_apex_data_path.c_str(), kReplace);
+
+    std::string dalvik_cache_dir = art_apex_data_path + "/dalvik-cache";
+    ASSERT_TRUE(EnsureDirectoryExists(dalvik_cache_dir));
+
+    std::string framework_dir = android_root_path + "/framework";
+    framework_jar_ = framework_dir + "/framework.jar";
+    location_provider_jar_ = framework_dir + "/com.android.location.provider.jar";
+    services_jar_ = framework_dir + "/services.jar";
+    std::string services_jar_prof = framework_dir + "/services.jar.prof";
+    std::string javalib_dir = android_art_root_path + "/javalib";
+    std::string boot_art = javalib_dir + "/boot.art";
+
+    // Create placeholder files.
+    ASSERT_TRUE(EnsureDirectoryExists(framework_dir));
+    CreateEmptyFile(framework_jar_);
+    CreateEmptyFile(location_provider_jar_);
+    CreateEmptyFile(services_jar_);
+    CreateEmptyFile(services_jar_prof);
+    ASSERT_TRUE(EnsureDirectoryExists(javalib_dir));
+    CreateEmptyFile(boot_art);
+
+    config_.SetApexInfoListFile(Concatenate({temp_dir_path, "/apex-info-list.xml"}));
+    config_.SetArtBinDir(Concatenate({temp_dir_path, "/bin"}));
+    config_.SetBootClasspath(framework_jar_);
+    config_.SetDex2oatBootclasspath(framework_jar_);
+    config_.SetSystemServerClasspath(Concatenate({location_provider_jar_, ":", services_jar_}));
+    config_.SetIsa(InstructionSet::kX86_64);
+    config_.SetZygoteKind(ZygoteKind::kZygote64_32);
+
+    std::string staging_dir = dalvik_cache_dir + "/staging";
+    ASSERT_TRUE(EnsureDirectoryExists(staging_dir));
+    config_.SetStagingDir(staging_dir);
+
+    auto mock_exec_utils = std::make_unique<MockExecUtils>();
+    mock_exec_utils_ = mock_exec_utils.get();
+
+    metrics_ = std::make_unique<OdrMetrics>(dalvik_cache_dir);
+    odrefresh_ = std::make_unique<OnDeviceRefresh>(
+        config_, dalvik_cache_dir + "/cache-info.xml", std::move(mock_exec_utils));
+  }
+
+  void TearDown() override {
+    metrics_.reset();
+    temp_dir_.reset();
+    android_root_env_.reset();
+    android_art_root_env_.reset();
+    art_apex_data_env_.reset();
+
+    CommonArtTest::TearDown();
+  }
+
+  std::unique_ptr<ScratchDir> temp_dir_;
+  std::unique_ptr<ScopedUnsetEnvironmentVariable> android_root_env_;
+  std::unique_ptr<ScopedUnsetEnvironmentVariable> android_art_root_env_;
+  std::unique_ptr<ScopedUnsetEnvironmentVariable> art_apex_data_env_;
+  OdrConfig config_;
+  MockExecUtils* mock_exec_utils_;
+  std::unique_ptr<OdrMetrics> metrics_;
+  std::unique_ptr<OnDeviceRefresh> odrefresh_;
+  std::string framework_jar_;
+  std::string location_provider_jar_;
+  std::string services_jar_;
+};
+
+TEST_F(OdRefreshTest, OdrefreshArtifactDirectory) {
+  // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
+  // where compilation artifacts end up.
+  ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
+  EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
+}
+
+TEST_F(OdRefreshTest, CompileSetsCompilerFilter) {
+  {
+    // Defaults to "speed".
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Not(Contains(HasSubstr("--profile-file-fd="))),
+                                  Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Not(Contains(HasSubstr("--profile-file-fd="))),
+                                          Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
+
+  {
+    auto guard = ScopedSetProperty("dalvik.vm.systemservercompilerfilter", "speed-profile");
+    // services.jar has a profile, while location.provider.jar does not.
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Not(Contains(HasSubstr("--profile-file-fd="))),
+                                  Contains("--compiler-filter=speed"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Contains(HasSubstr("--profile-file-fd=")),
+                                          Contains("--compiler-filter=speed-profile"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
+
+  {
+    auto guard = ScopedSetProperty("dalvik.vm.systemservercompilerfilter", "verify");
+    EXPECT_CALL(
+        *mock_exec_utils_,
+        DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", location_provider_jar_})),
+                                  Not(Contains(HasSubstr("--profile-file-fd="))),
+                                  Contains("--compiler-filter=verify"))))
+        .WillOnce(Return(0));
+    EXPECT_CALL(*mock_exec_utils_,
+                DoExecAndReturnCode(AllOf(Contains(Concatenate({"--dex-file=", services_jar_})),
+                                          Not(Contains(HasSubstr("--profile-file-fd="))),
+                                          Contains("--compiler-filter=verify"))))
+        .WillOnce(Return(0));
+    EXPECT_EQ(odrefresh_->Compile(
+                  *metrics_, /*compile_boot_extensions=*/{}, /*compile_system_server=*/true),
+              ExitCode::kCompilationSuccess);
+  }
 }
 
 }  // namespace odrefresh
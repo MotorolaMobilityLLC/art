4648df13da12ea522e0b729014fbdd3a1d188fa0
Guo Li <guo.li@mediatek.com>
2023-03-31 14:23:46 +0800

    [ALPS08003313] ART: revert "Clear interpreter cache with atomic stores"
    [Detail]
    This change will cause many boot exception.
    We already report to google, and google found this issue also.
    For avoid much more boot exception, we revert this change first.
    
    [Solution]
    Revert "Clear interpreter cache with atomic stores"
    
    CR-Id: ALPS08003313
    Feature: ART Android Runtime
    Signed-off-by: Guo Li <guo.li@mediatek.com>
    Change-Id: I550a948d02a8f2c1bee15d8a211a5819bad10ed2

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -4488,8 +4488,7 @@ static void SweepCacheEntry(IsMarkedVisitor* visitor, const Instruction* inst, s
       mirror::Object* new_object = visitor->IsMarked(object);
       // We know the string is marked because it's a strongly-interned string that
       // is always alive (see b/117621117 for trying to make those strings weak).
-      CHECK_NE(new_object, nullptr) << "old-string:" << object;
-      if (new_object != object) {
+      if (new_object != nullptr && new_object != object) {
         *value = reinterpret_cast<size_t>(new_object);
       }
       return;
